608e47824b7da72743b163752cb7925813715798
Harsh Agrawal <itsharshag@gmail.com>
2023-09-13 00:16:01 +0530

    Adjust summary prompt

--- src/lib/prompts.ts
+++ src/lib/prompts.ts
@@ -2,17 +2,7 @@ export const SUMMARY_PROMPT = `Below is the transcript of a customer satisfactio
 
 Please separate the both parts and return everything in markdown format. "Quantitative Part" and "Qualitative Part" should be formatted as headings.
 Quantitative part should be in list format with proper serial numbers and any required sublists. 
-Qualitative part should have the answers to all the open-ended questions asked in the second half of the interview, these questions are like these:
-What specific recommendations do you have regarding how our company could work more effectively with your company?
-What additional products or services would you like to see our company offer?
-What do you believe to be the most important issue that our company should address for your company in the coming year?
-Make sure the answer to all of the above three questions are included in as much detail as possible in the Qualitative part.
-More instructions for the qualitative part:
-Keep as much of the text of the original answers possible. Do very minimal polishing and shortening.
-It should be in the perspective of the customer and in strict first person. Use sentences like: I think they need to improve in. I had a great experience with them. 
-Do not repeat or include what was already mentioned in the Quantitative part. Remove any fluff or pleasantries.
-Don't add opening or closing notes and summaries. 
-Include every single important and specific detail like names, places, incidents, reasons, etc. 
+Qualitative part should be a slightly trimmed down version of the transcript with any pleasantries and trivial information removed. Keep the wordings and sentences close to the original transcript.
 
 Example Quantitative part:
 XYZ has the requisite software engineering expertise to meet your
@@ -26,5 +16,8 @@ Generally, their communication is very good. We have good personal contact with 
 I don’t have an answer to that.
 
 The area of focus relates to my earlier comment about needing to show models' interiors, not just the surfaces. We’re looking to accelerate our integration with other products, so we need to have both surfaces and interiors. It would be helpful if they could solve the issue of rendering interiors.
+Make sure the qualitative part is very very detailed.
 
 Transcript:`;
+
+console.log({ SUMMARY_PROMPT });
